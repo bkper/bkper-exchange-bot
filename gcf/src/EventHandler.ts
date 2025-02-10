4d81db830ef9f9b474126fd3a1aa5c50ce217f44
victordrumond <victordrumond@hotmail.com>
2025-02-10 10:21:10 -0300

    Prevent unnecessary early fetch of exchange rates

--- gcf/src/EventHandler.ts
+++ gcf/src/EventHandler.ts
@@ -1,6 +1,6 @@
-import { Bkper, Book } from "bkper-js";
-import { getBaseCode, getConnectedBooks, getRatesEndpointConfig, isBaseBook } from "./BotService.js";
-import {  EXC_ON_CHECK_PROP, EXC_CODE_PROP } from "./constants.js";
+import { Book } from "bkper-js";
+import { getBaseCode, getConnectedBooks, getRatesEndpointConfig } from "./BotService.js";
+import { EXC_ON_CHECK_PROP, EXC_CODE_PROP } from "./constants.js";
 import { getRates } from "./exchange-service.js";
 
 export abstract class EventHandler {
@@ -8,36 +8,47 @@ export abstract class EventHandler {
   protected abstract processObject(baseBook: Book, connectedBook: Book, event: bkper.Event): Promise<string>;
 
   async handleEvent(event: bkper.Event): Promise<string[] | string | boolean> {
-    let baseBook = new Book(event.book);
-    let baseCode = getBaseCode(baseBook);
 
-    if (baseCode == null || baseCode == '') {
-      return `Please set the "${EXC_CODE_PROP}" property of this book.`
+    const eventBook = new Book(event.book);
+    const eventBookExcCode = getBaseCode(eventBook);
+
+    if (eventBookExcCode == null || eventBookExcCode == '') {
+      return `Please set the "${EXC_CODE_PROP}" property of this book.`;
     }
 
-    const logtag = `Handling ${event.type} event on book ${baseBook.getName()} from user ${event.user.username} ${Math.random()}`;
-
-    console.time(logtag)
+    const logtag = `Handling ${event.type} event on book ${eventBook.getName()} from user ${event.user.username} ${Math.random()}`;
+    console.time(logtag);
 
     if (event.type == 'TRANSACTION_POSTED') {
       let operation = event.data.object as bkper.TransactionOperation;
       let transaction = operation.transaction;
-      if (baseBook.getProperty(EXC_ON_CHECK_PROP, 'exc_auto_check') && !transaction.checked) {
+      if (eventBook.getProperty(EXC_ON_CHECK_PROP, 'exc_auto_check') && !transaction.checked) {
         return false;
       }
     }
 
-    if (event.type == 'TRANSACTION_CHECKED' || event.type == 'TRANSACTION_POSTED' || event.type == 'TRANSACTION_UPDATED') {
-      //Load and cache rates prior to pararllel run
-      let operation = event.data.object as bkper.TransactionOperation;
-      let transaction = operation.transaction;
+    let allConnectedBooks = await getConnectedBooks(eventBook);
 
-      let ratesEndpointConfig = getRatesEndpointConfig(baseBook, transaction);
-      //Call to put rates on cache
-      await getRates(ratesEndpointConfig.url)
+    // No connected books
+    if (allConnectedBooks == null || allConnectedBooks.length == 0) {
+      return false;
+    }
+    // Event book is the only connected book
+    if (allConnectedBooks.length == 1) {
+      const connectedBook = allConnectedBooks[0];
+      if (connectedBook == null || connectedBook.getId() == eventBook.getId()) {
+        return false;
+      }
     }
 
-    let allConnectedBooks = await getConnectedBooks(baseBook);
+    // Load and cache rates prior to pararllel run
+    if (event.type == 'TRANSACTION_CHECKED' || event.type == 'TRANSACTION_POSTED' || event.type == 'TRANSACTION_UPDATED') {
+      let operation = event.data.object as bkper.TransactionOperation;
+      let transaction = operation.transaction;
+      const ratesEndpointConfig = getRatesEndpointConfig(eventBook, transaction);
+      // Call to put rates on cache
+      await getRates(ratesEndpointConfig.url); 
+    }
 
     let result: string[] = [];
 
@@ -48,12 +59,12 @@ export abstract class EventHandler {
       let responsesPromises: Promise<string>[] = [];
 
       for (const connectedBook of connectedBooks) {
-        if (connectedBook.getId() == baseBook.getId()) {
+        if (connectedBook.getId() == eventBook.getId()) {
           continue;
         }
         let connectedCode = getBaseCode(connectedBook);
         if (connectedCode != null && connectedCode != "") {
-          let response = this.processObject(baseBook, connectedBook, event);
+          let response = this.processObject(eventBook, connectedBook, event);
           if (response) {
             responsesPromises.push(response);
           }
@@ -79,7 +90,6 @@ export abstract class EventHandler {
 
     return result;
   }
-
 
   protected buildBookAnchor(book: Book) {
     return `<a href='https://app.bkper.com/b/#transactions:bookId=${book.getId()}'>${book.getName()}</a>`;
